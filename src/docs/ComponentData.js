84926a020f28d5b2f0c34e1aff9585be0f46d427
Ajaykumar <pajay2507@gmail.com>
2017-12-21 16:34:39 -0500

    added props and example component

--- src/docs/ComponentData.js
+++ src/docs/ComponentData.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-
+import Props from './Props';
+import Example from './Example';
 
 class ComponentData extends Component{
 
     render(){
-        const { name, props } = this.props.component;
+        const { name, props,examples } = this.props.component;
         return(
             <div>
-                Component data testing...
+                <h3>Example { examples.length > 0} </h3>
+                {
+                    examples.length > 0 ? examples.map(example => <Example key={example.code} example={example} componentName={name} />) :
+                        'No Examples exist.'
+                }
+                <h3>Props</h3>
+                {props ?
+                    <Props props = {props} /> : 'No props for this component' }
             </div>
         )
     }
---
+++ src/docs/Example.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CodeRefractor from './CodeRefractor';
+
+class Example extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("inside example");
+        console.log(this.props);
+        this.state = {
+            showCode: false
+        };
+    }
+
+    toggleCode(event) {
+        event.preventDefault();
+        this.setState(prevState => {
+            return {showCode: !prevState.showCode};
+        });
+    }
+
+    render() {
+        const {name, code, description} = this.props.example;
+        const {showCode} = this.state;
+        const ExampleComponent = require(`./examples/${this.props.componentName}/${name}`).default;
+        return (
+            <div className="example-wrapper">
+                <p>
+                    {description}
+                </p>
+
+                <ExampleComponent />
+                <a href="#" onClick={this.toggleCode.bind(this)}>
+                    {showCode ? "Hide" : "Show"} Code</a>
+                {showCode && <CodeRefractor>{code}</CodeRefractor>}
+            </div>
+        )
+    }
+}
+
+Example.propTypes = {
+    example: PropTypes.object.isRequired,
+    componentName: PropTypes.string.isRequired
+}
+
+export default Example;
