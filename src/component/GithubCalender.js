d2ec8db0f42baa04e628054f60751c4238fd39df
Ajaykumar <pajay2507@gmail.com>
2017-12-20 21:29:58 -0500

    modified props and css

--- src/component/GithubCalender.js
+++ src/component/GithubCalender.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './GithubCalender.css';
-
 class GithubCalender extends Component{
 
-    constructor(props){
-        super(props);
-        console.log(this.props);
-        this.state = {
-            data: ''
-        }
-    }
+
 
 
     createGithubCalender(body){
@@ -18,31 +11,29 @@ class GithubCalender extends Component{
         let div = document.createElement("div");
         div.innerHTML = body;
         let cal = div.querySelector(".js-contribution-graph");
-        cal.querySelector(".float-left.text-gray").innerHTML = "Ajay kumar";
-        /*this.setState({
-            data: cal.innerHTML
-        })*/
+        let hyperlink = "<a href='http://github.com/'+this.props.username>"+this.props.username+"</a>";
+        cal.querySelector(".float-left.text-gray").innerHTML = this.props.text || "Contributed by "+hyperlink;
         document.getElementById('react-github-id').innerHTML = cal.innerHTML;
 
     }
 
+
+
     componentDidMount(){
-        console.log("component did mount");
-        console.log(this.props);
         //proxy for cors
         let fetchCalendar = () => fetch("https://urlreq.appspot.com/req?method=GET&url=https://github.com/" + this.props.username).then(response => {
             console.log("success");
             return response.text()
         }).then(body => {
-           console.log("body"+body);
-           this.createGithubCalender(body);
+            console.log("body"+body);
+            this.createGithubCalender(body);
         }).catch(e => console.error(e));
         fetchCalendar();
     }
 
     render(){
         return(
-            <div id="react-github-id"></div>
+            <div className="github-calendar" id="react-github-id"></div>
         )
     }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './component/GithubCalender.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import GithubCalender from './component/GithubCalender';
 
-ReactDOM.render(<GithubCalender username='ajay2507' />, document.getElementById('root'));
+ReactDOM.render(<GithubCalender username="ajay2507" text="All commits done by" />, document.getElementById('root'));
 registerServiceWorker();
