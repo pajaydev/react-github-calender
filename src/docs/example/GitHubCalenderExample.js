18fd439661c1c6e92415955a5c5c0e068ea51d9c
Ajaykumar <pajay2507@gmail.com>
2017-12-20 22:59:20 -0500

    included github calender example

---
+++ src/docs/example/GitHubCalenderExample.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import GithubCalender from '../../component/GithubCalender';
+
+/* Example for Github Calender */
+export default function GithubCalenderExample() {
+    return <GithubCalender username="ajay2507" text="All commits done by" />
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './component/GithubCalender.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import GithubCalender from './component/GithubCalender';
+import App from './docs/App';
 
-ReactDOM.render(<GithubCalender username="ajay2507" text="All commits done by" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
